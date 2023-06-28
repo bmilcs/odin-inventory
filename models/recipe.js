13db41941112009ec0d0efea3c8822378fc1d028
Bryan Miller <bmilcs@yahoo.com>
2023-06-28 16:59:15 -0400

    Add recipe model

---
+++ models/recipe.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 1000,
+  },
+  ingredients: [
+    {
+      ingredient: {
+        type: Schema.Types.ObjectId,
+        ref: "Ingredient",
+      },
+      measurement: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  instructions: [{ step: Number, description: String }],
+  time: {
+    type: Number,
+    required: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
