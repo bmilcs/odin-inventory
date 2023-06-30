ae75c3ba004c150c7ddeb5ca20a89d1029628b0a
Bryan Miller <bmilcs@yahoo.com>
2023-06-30 15:34:53 -0400

    Add populatedb script with dummy db entries

--- populatedb.js
+++ populatedb.js
@@ -3,201 +3,116 @@
 // Get arguments passed on command line
 const userArgs = process.argv.slice(2);
 
-const Book = require("./models/book");
-const Author = require("./models/author");
-const Genre = require("./models/genre");
-const BookInstance = require("./models/bookinstance");
+const Product = require("./models/product");
+const Category = require("./models/category");
 
-const genres = [];
-const authors = [];
-const books = [];
-const bookinstances = [];
+const categories = [];
+const products = [];
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false); // Prepare for Mongoose 7
-
 const mongoDB = userArgs[0];
 
 main().catch(err => console.log(err));
-
 async function main() {
   console.log("Debug: About to connect");
   await mongoose.connect(mongoDB);
   console.log("Debug: Should be connected?");
-  await createGenres();
-  await createAuthors();
-  await createBooks();
-  await createBookInstances();
+  await createAllCategories();
+  await createAllProducts();
   console.log("Debug: Closing mongoose");
   mongoose.connection.close();
 }
 
-async function genreCreate(name) {
-  const genre = new Genre({ name: name });
-  await genre.save();
-  genres.push(genre);
-  console.log(`Added genre: ${name}`);
+async function createAllCategories() {
+  console.log("Adding Categories");
+  await Promise.all([
+    createCategory("Sports", "Sporting Goods & Equipment"),
+    createCategory("Electronics", "Computers, TVs, Phones, etc"),
+    createCategory("Toys", "Children's Toys & Games"),
+  ]);
 }
 
-async function authorCreate(first_name, family_name, d_birth, d_death) {
-  const authordetail = { first_name: first_name, family_name: family_name };
-  if (d_birth != false) authordetail.date_of_birth = d_birth;
-  if (d_death != false) authordetail.date_of_death = d_death;
-
-  const author = new Author(authordetail);
-
-  await author.save();
-  authors.push(author);
-  console.log(`Added author: ${first_name} ${family_name}`);
+async function createAllProducts() {
+  console.log("Adding Products");
+  await Promise.all([
+    createProduct(
+      "SportPro Basketball",
+      "Experience the thrill of the game with the SportPro Basketball. This high-quality basketball is made with durable materials, ensuring excellent grip and bounce. Whether you're playing a friendly match or a competitive game, this basketball is designed to enhance your performance and provide an amazing playing experience.",
+      11.99,
+      10,
+      categories[0],
+    ),
+    createProduct(
+      "EliteStrike Football",
+      "Prepare for an exciting match with the EliteStrike Football. Crafted with precision, this top-notch football delivers exceptional durability and superior performance on any field. Its advanced design ensures excellent grip and accurate throws, making it perfect for practice sessions and professional games.",
+      14.99,
+      8,
+      categories[0],
+    ),
+    createProduct(
+      "ProShot Tennis Racket",
+      "Elevate your tennis game with the ProShot Tennis Racket. This advanced racket is engineered for power and control, allowing you to dominate the court. With its comfortable grip and excellent maneuverability, you'll experience enhanced precision and agility in every swing.",
+      29.99,
+      5,
+      categories[0],
+    ),
+    createProduct(
+      "TechMaster Laptop",
+      "Unleash your productivity and entertainment potential with the TechMaster Laptop. Featuring a sleek design and powerful performance, this laptop is packed with cutting-edge features. The high-resolution display and fast processor ensure smooth multitasking and stunning visuals. With ample storage space and long-lasting battery life, it's the perfect companion for work and play.",
+      899.99,
+      3,
+      categories[1],
+    ),
+    createProduct(
+      "SuperNova Smartphone",
+      "Stay connected and organized with the SuperNova Smartphone. Designed with a brilliant display and lightning-fast performance, this smartphone offers a seamless user experience. Capture stunning photos with its advanced camera system and enjoy immersive multimedia with its high-quality audio and visuals. With its sleek design and innovative features, the SuperNova keeps you ahead of the curve.",
+      699.99,
+      5,
+      categories[1],
+    ),
+    createProduct(
+      "RoboBuddy Robot Toy",
+      "Ignite your child's imagination with the RoboBuddy Robot Toy. This interactive robot companion is packed with exciting features and educational games. It promotes learning and creativity while providing hours of fun. With its friendly design and intuitive controls, RoboBuddy is the perfect playmate for your little one.",
+      39.99,
+      10,
+      categories[2],
+    ),
+    createProduct(
+      "FunLand Playset",
+      "Let your child's imagination run wild with the FunLand Playset. This versatile playset features a combination of slides, swings, and climbing structures, providing endless entertainment. It's made of durable materials and designed with safety in mind. Create unforgettable playtime memories with this fantastic playset.",
+      79.99,
+      7,
+      categories[2],
+    ),
+    createProduct(
+      "LaserBlast Nerf Gun",
+      "Gear up for epic battles with the LaserBlast Nerf Gun. This high-performance blaster features rapid-fire action and precision targeting. It comes with adjustable settings for different game modes, ensuring thrilling and competitive play. Get ready to blast away with this exciting Nerf gun!",
+      24.99,
+      15,
+      categories[2],
+    ),
+  ]);
 }
 
-async function bookCreate(title, summary, isbn, author, genre) {
-  const bookdetail = {
-    title: title,
-    summary: summary,
-    author: author,
-    isbn: isbn,
+async function createCategory(name, description) {
+  const category = new Category({ name: name, description: description });
+  await category.save();
+  categories.push(category);
+  console.log(`Added Category: ${name}`);
+}
+
+async function createProduct(name, description, price, quantity, category) {
+  const productDetail = {
+    name: name,
+    description: description,
+    price: price,
+    quantity: quantity,
   };
-  if (genre != false) bookdetail.genre = genre;
+  if (category != false) productDetail.category = category;
 
-  const book = new Book(bookdetail);
-  await book.save();
-  books.push(book);
-  console.log(`Added book: ${title}`);
-}
-
-async function bookInstanceCreate(book, imprint, due_back, status) {
-  const bookinstancedetail = {
-    book: book,
-    imprint: imprint,
-  };
-  if (due_back != false) bookinstancedetail.due_back = due_back;
-  if (status != false) bookinstancedetail.status = status;
-
-  const bookinstance = new BookInstance(bookinstancedetail);
-  await bookinstance.save();
-  bookinstances.push(bookinstance);
-  console.log(`Added bookinstance: ${imprint}`);
-}
-
-async function createGenres() {
-  console.log("Adding genres");
-  await Promise.all([
-    genreCreate("Fantasy"),
-    genreCreate("Science Fiction"),
-    genreCreate("French Poetry"),
-  ]);
-}
-
-async function createAuthors() {
-  console.log("Adding authors");
-  await Promise.all([
-    authorCreate("Patrick", "Rothfuss", "1973-06-06", false),
-    authorCreate("Ben", "Bova", "1932-11-8", false),
-    authorCreate("Isaac", "Asimov", "1920-01-02", "1992-04-06"),
-    authorCreate("Bob", "Billings", false, false),
-    authorCreate("Jim", "Jones", "1971-12-16", false),
-  ]);
-}
-
-async function createBooks() {
-  console.log("Adding Books");
-  await Promise.all([
-    bookCreate(
-      "The Name of the Wind (The Kingkiller Chronicle, #1)",
-      "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
-      "9781473211896",
-      authors[0],
-      [genres[0]],
-    ),
-    bookCreate(
-      "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-      "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
-      "9788401352836",
-      authors[0],
-      [genres[0]],
-    ),
-    bookCreate(
-      "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-      "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
-      "9780756411336",
-      authors[0],
-      [genres[0]],
-    ),
-    bookCreate(
-      "Apes and Angels",
-      "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
-      "9780765379528",
-      authors[1],
-      [genres[1]],
-    ),
-    bookCreate(
-      "Death Wave",
-      "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-      "9780765379504",
-      authors[1],
-      [genres[1]],
-    ),
-    bookCreate(
-      "Test Book 1",
-      "Summary of test book 1",
-      "ISBN111111",
-      authors[4],
-      [genres[0], genres[1]],
-    ),
-    bookCreate(
-      "Test Book 2",
-      "Summary of test book 2",
-      "ISBN222222",
-      authors[4],
-      false,
-    ),
-  ]);
-}
-
-async function createBookInstances() {
-  console.log("Adding authors");
-  await Promise.all([
-    bookInstanceCreate(books[0], "London Gollancz, 2014.", false, "Available"),
-    bookInstanceCreate(books[1], " Gollancz, 2011.", false, "Loaned"),
-    bookInstanceCreate(books[2], " Gollancz, 2015.", false, false),
-    bookInstanceCreate(
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available",
-    ),
-    bookInstanceCreate(
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available",
-    ),
-    bookInstanceCreate(
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available",
-    ),
-    bookInstanceCreate(
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
-      false,
-      "Available",
-    ),
-    bookInstanceCreate(
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
-      false,
-      "Maintenance",
-    ),
-    bookInstanceCreate(
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
-      false,
-      "Loaned",
-    ),
-    bookInstanceCreate(books[0], "Imprint XXX2", false, false),
-    bookInstanceCreate(books[1], "Imprint XXX3", false, false),
-  ]);
+  const product = new Product(productDetail);
+  await product.save();
+  products.push(product);
+  console.log(`Added product: ${name}`);
 }
